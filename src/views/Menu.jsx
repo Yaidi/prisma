6bedba318f96d4acd163fc4d8e8a899d67bcf447
Yaidi <yaidi.garcia.lopez@gmail.com>
2020-08-22 13:57:21 -0500

    header and files with header

--- src/views/Menu.jsx
+++ src/views/Menu.jsx
@@ -2,16 +2,19 @@ import React from 'react';
 import CardHeroine from '../components/CardHeroine';
 import { arrayHeroines } from '../data/arrayHeroines';
 import Style from '../styles/CardHeroine.module.css';
+import Header from '../components/Header';
 
 const Menu = ()=>{
     return(
-        <div className={`container-fluid row`}>
-            <h1>Escoge a tu heroína</h1> 
+        <div>
+            <Header title='Elige a tu heroína'/>
+        <div className={`container-fluid row ${Style.container}`}>
           {arrayHeroines.map((heroine, index)=>
             <section className={`${Style.card} col-md-6 col-lg-4 my-3`} style={{width: '18rem'}}  key={index}>
             <CardHeroine image= {heroine.image} name={heroine.name} description={heroine.description}/>
         </section>
         )}
+        </div>
         </div>
     )
 }
